ee30e08ca4c8623a9c553dca76dedabf86841b4b
Brent Groves <brent.groves@gmail.com>
2016-10-12 14:23:18 -0400

    first commit

---
+++ tbdel.bat
@@ -0,0 +1,5 @@
+del /Q c:\toolboss3\sps.mdb
+del /Q c:\toolboss6\sps.mdb
+del /Q c:\toolboss7\sps.mdb
+
+
---
+++ tbget.bat
@@ -0,0 +1,8 @@
+mkdir  c:\toolboss3
+COPY /V /Y  \\Toolboss-plt-3\Database\sps.mdb C:\toolboss3\sps.mdb
+mkdir  c:\toolboss6
+COPY /V /Y  \\TOOLBOSS-PLT6\Database\sps.mdb C:\toolboss6\sps.mdb
+mkdir  c:\toolboss7
+COPY /V /Y  \\TOOLBOSS-PLT7\Database\sps.mdb C:\toolboss7\sps.mdb
+
+
